21cb382e754f032dc774932c9014361f77003644
rikzaafnan <rikzaafnan@gmail.com>
2021-03-23 17:43:02 +0700

    add route and server

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 
-	fmt.Println("testing")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", helloHandler)
+
+	log.Println("Starting web on port 80808")
+
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Hello world, saya sedang belajr golang Web"))
 
 }
