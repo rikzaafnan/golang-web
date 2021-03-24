10ce36bd23903802bf4334e366674f09465de5c3
rikzaafnan <rikzaafnan@gmail.com>
2021-03-24 14:35:20 +0700

    add routing in main.go

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 
 	mux := http.NewServeMux()
 
+	// abouthandler := func(w http.ResponseWriter, r *http.Request) {
+
+	// 	w.Write([]byte("About Page"))
+
+	// }
+
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/hello", helloHandler)
 	mux.HandleFunc("/mario", marioHandler)
+	mux.HandleFunc("/product", productHandler)
+	// mux.HandleFunc("/about", abouthandler)
+	// mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Profile"))
+	// })
 
 	log.Println("Starting web on port 80808")
 
@@ -34,11 +47,31 @@ func marioHandler(w http.ResponseWriter, r *http.Request) {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
+	log.Println(r.URL.Path)
+
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
 	w.Write([]byte("welcome to root"))
+
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+
+	id := r.URL.Query().Get("id")
+
+	idNumb, err := strconv.Atoi(id)
+
+	if err != nil || idNumb < 1 {
+		http.NotFound(w, r)
+
+		return
+	}
+
+	// w.Write([]byte("Product Page"))
+
+	fmt.Fprintf(w, "Product Page : %d", idNumb)
 
 }
